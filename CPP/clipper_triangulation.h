a7046cbefbfa238e2ecabf90e77cae8709ec0c9a
Dennis Nienhüser <nienhueser@kde.org>
2018-05-17 21:25:10 +0200

    Fix typos in header

--- CPP/clipper_triangulation.h
+++ CPP/clipper_triangulation.h
@@ -1,11 +1,11 @@
 /*******************************************************************************
 * Author    :  Angus Johnson                                                   *
 * Version   :  10.0 (beta)                                                     *
-* Date      :  8 Noveber 2017                                                  *
+* Date      :  8 November 2017                                                 *
 * Website   :  http://www.angusj.com                                           *
 * Copyright :  Angus Johnson 2010-2017                                         *
 * Purpose   :  Triangulate clipping solutions                                  *
-* License   : http://www.boost.org/LICENSE_1_0.txt                             *
+* License   :  http://www.boost.org/LICENSE_1_0.txt                            *
 *******************************************************************************/
 
 #ifndef clipper_tri_h
